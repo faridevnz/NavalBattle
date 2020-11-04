141b7d6466d75b3b7782f382d54b2d85d65df64a
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-11-04 16:19:21 +0100

    last visualization works (#4)

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -2,20 +2,7 @@ import { Input, Output } from '@angular/core';
 import { Component, EventEmitter, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 
-interface Neighbors {
-  top: boolean,
-  right: boolean,
-  bottom: boolean,
-  left: boolean
-}
-interface boxState {
-  busy: boolean,
-  destroyed: boolean,
-  alignQueue: string[]
-}
 
-
-// Component
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -23,23 +10,24 @@ interface boxState {
 })
 export class GridComponent implements OnInit {
 
+  // props
   @Input() disableClick: boolean = false
   @Input() board = null
+  @Input() type: string // my or opposite
+  @Input() latest: number = null
+  @Input() defaultAlign: string
+  // event emitter
   @Output() onBoxAdded: EventEmitter<number> = new EventEmitter<number>()
   @Output() onOppositClick: EventEmitter<number> = new EventEmitter<number>()
-  @Input() type: string // my or opposite
-  alignes = [];
 
 
-  constructor(private firestore: AngularFirestore) {
-  }
+  constructor(private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
-    for (let i = 0; i < 100; i++) {
-      if ( this.type === 'my' )  this.alignes.push('center')
-      else this.alignes.push(null) 
-    }
+  
   }
+
+  // FUNCTIONS
 
   clicked(index: number): void {
     if (this.type === 'opposite') {
@@ -54,29 +42,6 @@ export class GridComponent implements OnInit {
       else this.onBoxAdded.emit(-1)
       this.board[index].busy = !this.board[index].busy
     }
-  }
-
-  neighborsCheck(index: number): void {
-    // conditionally adding center
-    if (!this.board[index].alignQueue.includes('center'))
-      this.board[index].alignQueue.push('center')
-    // top check
-    if (index > 9 && this.board[index - 10].busy && !this.board[index].alignQueue.includes('top'))
-      this.board[index].alignQueue.push('top')
-    // right check
-    if ((index - 1) % 10 !== 0 && this.board[index + 1].busy && !this.board[index].alignQueue.includes('right'))
-      this.board[index].alignQueue.push('right')
-    // bottom check
-    if (index < 49 && this.board[index + 10].busy && !this.board[index].alignQueue.includes('bottom'))
-      this.board[index].alignQueue.push('bottom')
-    // left check
-    if (index % 10 !== 0 && this.board[index - 1].busy && !this.board[index].alignQueue.includes('left'))
-      this.board[index].alignQueue.push('left')
-    // horizontal line check
-    if (!this.board[index].alignQueue.includes('line-horizontal'))
-      this.board[index].alignQueue.push('line-horizontal')
-    if (!this.board[index].alignQueue.includes('line-vertical'))
-      this.board[index].alignQueue.push('line-vertical')
   }
 
 }
